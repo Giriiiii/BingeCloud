d61f3876ac7cf3aa3b0d347a0fef9e73c470ff65
Girikrishna Gireeshkumar <girikrishnagireeshkumar@Girikrishnas-Laptop.local>
2024-07-20 16:46:19 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <footer className='text-center bg-neutral-600 bg-opacity-35 text-neutral-400 py-2'>
+        <div className='flex items-center justify-center gap-4'>
+          
+        </div>
+        <p className='text-sm'>BingeCloud</p>
+    </footer>
+  )
+}
+
+export default Footer
